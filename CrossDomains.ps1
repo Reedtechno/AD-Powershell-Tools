e35befe71f41c3ec38d946b8e40ad12a4eb5ed4b
DAVID REED <dreed@nhaschools.com>
2018-03-13 09:05:57 -0400

    Initial commit

---
+++ CrossDomains.ps1
@@ -0,0 +1,23 @@
+#NAME:  CrossDomains.ps1
+#AUTHOR: David Reed, @ReedTechno
+#LASTEDIT: 3/13/2018
+
+# Provide FQDN of remote host
+$RemoteHost = ''
+# Provide Remote Domain Credentials.  ie. domain\username
+$cred = Get-Credential
+
+
+# Sets your Remote Host as a trusted host in WinRM Client Config.  By default, the client will block WinRM access to host that is not on the same domain.
+Set-WSManInstance -ResourceURI winrm/config/client -ValueSet @{TrustedHosts=$RemoteHost}
+
+#Create a remote powershell session
+$sess = New-PSSession -ComputerName $RemoteHost -Credential $cred
+Enter-PSSession $sess
+
+
+
+
+# Close Session
+Exit-PSSession 
+
